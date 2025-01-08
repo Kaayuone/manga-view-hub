533054437ed190031dd7a7888654e4aeb01d1ed2
Kaayuone <kaayuone@gmail.com>
2025-01-08 16:09:01 +0300

    feat(server): add requests for frames, all chapters and proxy get image

--- server/src/types/common.ts
+++ server/src/types/common.ts
@@ -1,1 +1,9 @@
+import type { ChapterFrame } from '@project-common/types/common';
+
 export type WorkType = 'Манга' | 'Новелла';
+
+export interface ChapterResponse {
+  tome: number;
+  chapter: string;
+  pages: ChapterFrame[];
+}
