8f772404714e693daa787b5bec0a0bcf5f47f6b7
Kaayuone <kaayuone@gmail.com>
2025-03-04 20:08:07 +0300

    feat(server/user): update with refresh token

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,6 +2,8 @@ import * as argon2 from 'argon2';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { COMMON } from '@/constants';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
@@ -14,6 +16,57 @@ export class UsersService {
       data: {
         ...createUserDto,
         password: hash,
+      },
+    });
+  }
+
+  findOne(id?: number, username?: string, includeTokens?: boolean) {
+    if (!id && !username) throw new Error('no filter data provided');
+
+    return this.prisma.user.findUnique({
+      where: {
+        ...(id && { id }),
+        ...(username && { username }),
+      },
+      include: {
+        refreshToken: includeTokens,
+      },
+    });
+  }
+
+  async addRefreshToken(userId: number, refreshToken: string): Promise<void> {
+    const expiresAt = new Date(Date.now() + COMMON.REFRESH_TOKEN_EXPIRATION_MS);
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        refreshToken: {
+          create: {
+            token: refreshToken,
+            expiresAt,
+          },
+        },
+      },
+    });
+  }
+
+  async removeRefreshToken(userId: number, refreshToken: string): Promise<void> {
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+        refreshToken: {
+          some: {
+            token: refreshToken,
+          },
+        },
+      },
+      data: {
+        refreshToken: {
+          deleteMany: {
+            token: refreshToken,
+          },
+        },
       },
     });
   }
